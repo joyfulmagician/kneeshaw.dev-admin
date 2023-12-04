f17ffdb7287e60c43b7f81bc687153eaeb7e99a1
truthfuldev <truthfuldev@gmail.com>
2023-12-04 16:25:15 +0100

    complete job-scope

--- src/app/job/services/page.tsx
+++ src/app/job/services/page.tsx
@@ -8,39 +8,6 @@ export const metadata: Metadata = {
   description: `${SITE_TITLE} - Job Services`
 };
 
-const services = [
-  {
-    id: "1",
-    service: "Godot",
-    description: "description1"
-  },
-  {
-    id: "2",
-    service: "2D",
-    description: "description2"
-  },
-  {
-    id: "3",
-    service: "3D",
-    description: "description3"
-  },
-  {
-    id: "4",
-    service: "Shaders",
-    description: "description4"
-  },
-  {
-    id: "5",
-    service: "Environment",
-    description: "description5"
-  },
-  {
-    id: "6",
-    service: "System",
-    description: "description6"
-  }
-];
-
 export default function Services() {
   return (
     <section className="mt-[50px] flex flex-col gap-[20px]">
