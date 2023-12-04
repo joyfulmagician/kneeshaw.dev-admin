257a5a124739299ee1e081fc87855df262784e11
JoyfulMagician <joyfulmagician@gmail.com>
2023-12-04 22:20:54 +0100

    complete game

--- src/app/blog/create/page.tsx
+++ src/app/blog/create/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 
 import { SITE_TITLE } from "@/utils/constants";
 import CreateBlog from "@/components/blog/CreateBlog";
+
 export const metadata: Metadata = {
   title: `${SITE_TITLE} - Create Blog`,
   description: `${SITE_TITLE} - Create Blog`
