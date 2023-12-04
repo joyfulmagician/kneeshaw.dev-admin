ad41417dddf4ad95e479b2ccc32919fea9af433a
truthfuldev <truthfuldev@gmail.com>
2023-12-04 15:52:27 +0100

    complete job-database

--- src/app/job/databases/page.tsx
+++ src/app/job/databases/page.tsx
@@ -1,18 +1,7 @@
 import { Metadata } from "next";
 
+import JobDatabasesList from "@/components/job/databases/DatabaseList";
 import { SITE_TITLE } from "@/utils/constants";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow
-} from "@/components/ui/table";
-import { Button } from "@/components/ui/button";
-import { FaPlus } from "react-icons/fa";
-import { HiOutlinePencilSquare } from "react-icons/hi2";
-import { MdDeleteOutline } from "react-icons/md";
 
 export const metadata: Metadata = {
   title: `${SITE_TITLE} - Job Databases`,
@@ -55,44 +44,7 @@ const databases = [
 export default function Databases() {
   return (
     <section className="mt-[50px] flex flex-col gap-[20px]">
-      <div className="mr-[20px] flex justify-end">
-        <Button type="button">
-          <FaPlus className="text-md" />
-          <span className="ml-[5px] text-[16px]">Add</span>
-        </Button>
-      </div>
-
-      <Table className="text-center">
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[5%] text-center">ID</TableHead>
-            <TableHead className="w-[15%] text-center">Database</TableHead>
-            <TableHead className="w-[65%] text-center">Description</TableHead>
-            <TableHead colSpan={2} className="w-[15%] text-center">
-              Action
-            </TableHead>
-          </TableRow>
-        </TableHeader>
-
-        <TableBody>
-          {databases.map((database) => (
-            <TableRow key={database.id}>
-              <TableCell className="font-medium">{database.id}</TableCell>
-              <TableCell>{database.database}</TableCell>
-              <TableCell>{database.description}</TableCell>
-              <TableCell className="flex flex-row items-center justify-center gap-[20px]">
-                <Button className="bg-transparent hover:border hover:bg-transparent">
-                  <HiOutlinePencilSquare className="text-lg text-black" />
-                </Button>
-
-                <Button className="bg-transparent hover:border hover:bg-transparent">
-                  <MdDeleteOutline className="text-xl text-black" />
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <JobDatabasesList />
     </section>
   );
 }
