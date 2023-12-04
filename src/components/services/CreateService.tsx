72b6023b2ddb755fa31f95a897f87d35d04385fe
JoyfulMagician <joyfulmagician@gmail.com>
2023-12-04 19:01:25 +0100

    update service

--- src/components/services/CreateService.tsx
+++ src/components/services/CreateService.tsx
@@ -1,19 +1,29 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue
-} from "@/components/ui/select";
-import { Input } from "../ui/input";
 import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function CreateService() {
   return (
-    <div className="flex flex-col gap-[30px] p-[100px] text-[black]">
+    <div className="flex flex-col gap-[30px] text-[black]">
+      <div className="flex items-center gap-[16px]">
+        <span className="w-[100px] text-center">Image:</span>
+        <Input type="file" className="w-[500px]" />
+      </div>
+
+      <div className="flex items-center gap-[16px]">
+        <span className="w-[100px] text-center">Title:</span>
+        <Input type="text" className="w-[500px]" />
+      </div>
+
+      <div className="flex items-center gap-[16px]">
+        <span className="w-[100px] text-center">Description:</span>
+        <Textarea placeholder="Type your message here." id="message" />
+      </div>
+
       <div className="flex flex-row justify-end gap-[20px]">
         <Link href="/service">
           <Button variant="outline" className="w-[80px]">
@@ -23,23 +33,6 @@ export default function CreateService() {
         <Button type="button" className="w-[80px]">
           <span className="text-[16px]">Save</span>
         </Button>
-      </div>
-
-      <div className="flex flex-col gap-[12px]">
-        <div className="flex items-center gap-[18px]">
-          <p className="w-[100px] text-center">Image:</p>
-          <Input type="text" className="w-[500px]"></Input>
-        </div>
-
-        <div className="flex items-center gap-[18px]">
-          <p className="w-[100px] text-center">Title:</p>
-          <Input type="text" className="w-[500px]"></Input>
-        </div>
-
-        <div className="flex items-center gap-[18px]">
-          <p className="w-[100px] text-center">Description:</p>
-          <Input type="text" className="w-[500px]"></Input>
-        </div>
       </div>
     </div>
   );
