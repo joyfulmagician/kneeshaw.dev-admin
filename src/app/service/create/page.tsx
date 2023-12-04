7ad978c45f37988be98111c22c80cf2da91e4636
truthfuldev <truthfuldev@gmail.com>
2023-12-04 19:01:25 +0100

    update service

--- src/app/service/create/page.tsx
+++ src/app/service/create/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 
 import { SITE_TITLE } from "@/utils/constants";
 import CreateService from "@/components/services/CreateService";
+
 export const metadata: Metadata = {
   title: `${SITE_TITLE} - Service Create`,
   description: `${SITE_TITLE} - Service Create`
@@ -9,7 +10,7 @@ export const metadata: Metadata = {
 
 export default function AddService() {
   return (
-    <section className="p-[150px]">
+    <section>
       <CreateService />
     </section>
   );
